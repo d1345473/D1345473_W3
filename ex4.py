461cbce203490167a86289c11eab80b12887a5b8
d1345473 <d1345473@o365.fcu.edu.tw>
2024-09-26 15:49:13 +0800

    ex4.py

---
+++ ex4.py
@@ -0,0 +1,3 @@
+a=str(input('請輸入一個三位數:'))
+b=list(a)
+print('每位數字的總和:',int(b[0])+int(b[1])+int(b[2]))
--- 題目4.py
+++
@@ -1,3 +0,0 @@
-a=str(input('請輸入一個三位數:'))
-b=list(a)
-print('每位數字的總和:',int(b[0])+int(b[1])+int(b[2]))
